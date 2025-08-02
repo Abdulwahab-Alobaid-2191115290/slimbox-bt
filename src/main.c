bee9ad826d59928b1178316650fe6ea3633c6205
Abdulwahab Alobaid <81912560+Abdulwahab-Alobaid-2191115290@users.noreply.github.com>
2025-08-02 12:47:42 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,56 +1,26 @@
-#include <assert.h>
-#include <errno.h>
-#include <soc.h>
-#include <stddef.h>
-#include <string.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/settings/settings.h>
-#include <zephyr/sys/byteorder.h>
-#include <zephyr/sys/poweroff.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/reboot.h>
-#include <zephyr/types.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
 
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/gatt.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/uuid.h>
+#define LED0_NODE DT_ALIAS(led0)
 
-#include <bluetooth/services/hids.h>
-#include <zephyr/bluetooth/services/bas.h>
-#include <zephyr/bluetooth/services/dis.h>
-
-static void bt_ready(int err)
-{
-    if (err) {
-        printk("Bluetooth init failed (err %d)\n", err);
-        return;
-    }
-    printk("Bluetooth initialized\n");
-
-    /* Start advertising */
-    err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, NULL, 0, NULL, 0);
-    if (err) {
-        printk("Advertising failed to start (err %d)\n", err);
-        return;
-    }
-    printk("Advertising successfully started\n");
-}
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 void main(void)
 {
-    printk("Starting Bluetooth Peripheral example\n");
+    int ret;
 
-    int err = bt_enable(bt_ready);
-    if (err) {
-        printk("Bluetooth enable failed (err %d)\n", err);
+    if (!device_is_ready(led.port)) {
+        return;
     }
 
-    /* main loop */
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return;
+    }
+
+    // LED stays ON forever
     while (1) {
-        k_sleep(K_SECONDS(1));
+        k_sleep(K_FOREVER);
     }
 }
